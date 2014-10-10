b9c94a38cdf566412ef13a112839dc4f0cd4ead8
Celso Fernandes <fernandes@zertico.com>
2014-10-10 19:19:03 -0300

    Load component's dependencies automatically on wrapper
    Once you add a dependency to your component then you need to explicit require under
    lib/component/engine.rb
    
    This commit adds a small trick that load automatically all dependencies, so you just
    add the gem on your gemspec and voila, its automatically loaded on wrapper
    
    source: Stackoverflow question http://bit.ly/1oYVETX

--- components/component_template/lib/component_template/engine.rb
+++ components/component_template/lib/component_template/engine.rb
@@ -2,6 +2,16 @@ module ComponentTemplate
   class Engine < ::Rails::Engine
     isolate_namespace ComponentTemplate
     
+    # Initialize engine dependencies on wrapper application
+    Gem.loaded_specs["component_template"].dependencies.each do |d|
+      begin
+        require d.name
+      rescue LoadError => le
+        # Put exceptions here.
+        raise le if d.name !~ /factory_girl_rails/
+      end
+    end
+    
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
